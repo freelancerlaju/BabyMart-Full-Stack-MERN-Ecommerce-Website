bd0ec9accf655dcfa23a3190437fd4e13cb1f928
Freelancer Laju <fa.freelancerlaju@gmail.com>
2026-02-08 21:08:32 +0600

    fixed

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://babyshop-ecommerce.vercel.app/"
+  "https://babyshop-ecommerce.vercel.app"
 ].filter(Boolean);
 
 app.use(
